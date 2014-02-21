48623d792b578d87542cb66b9047bdee126b1de7
anseki <AnSeki.aff@gmail.com>
2014-02-21 20:17:21 +0900

    fix: cleanpdf.pl

--- cleanpdf.pl
+++ cleanpdf.pl
@@ -14,37 +14,19 @@ use PDF::API2;
 use CAM::PDF;
 
 my $path_src = $ARGV[0];
-unless (-f $path_src) {
-    print STDERR "Specify PDF file. $path_src\n";
-    exit 1;
-}
+_error("Specify PDF file. $path_src") unless -f $path_src;
 my $path_dest = "$path_src.dst.pdf";
 my $path_temp = "$path_src.tmp.pdf";
 
 my $pdf_src = eval { PDF::API2->open($path_src); };
 unless ($pdf_src) { # Try to convert version via CAM::PDF
-    my $campdf = eval { CAM::PDF->new($path_src); };
-    unless ($campdf) {
-        print STDERR "<CAM::PDF> Can't open. $path_src\n";
-        exit 1;
-    }
+    my $campdf = eval { CAM::PDF->new($path_src); } or _error("<CAM::PDF> Can't open. $path_src");
     # toPDF() and openScalar() can't pass to PDF::API2.
     $campdf->cleanoutput($path_temp);
-    $pdf_src = eval { PDF::API2->open($path_temp); };
-    unless ($pdf_src) {
-        print STDERR "<PDF::API2> Can't open. $path_temp\n";
-        unlink $path_dest, $path_temp;
-        exit 1;
-    }
+    $pdf_src = eval { PDF::API2->open($path_temp); } or _error("<PDF::API2> Can't open. $path_temp");
 }
 
-my $pages_count = $pdf_src->pages();
-unless ($pages_count) {
-    print STDERR "<PDF::API2> No page.\n";
-    unlink $path_dest, $path_temp;
-    exit 1;
-}
-
+my $pages_count = $pdf_src->pages() or _error('<PDF::API2> No page.');
 my $pdf_dest = PDF::API2->new(-onecolumn => 1);
 delete $pdf_dest->{pdf}->{Info};
 delete $pdf_dest->{catalog}->{PageLayout};
@@ -57,10 +39,14 @@ $pdf_src->end();
 $pdf_dest->end();
 
 unlink $path_src;
-unless (rename $path_dest, $path_src) {
-    print STDERR "rename ERROR: $!\n";
-    unlink $path_dest, $path_temp;
+rename $path_dest, $path_src or _error("rename ERROR: $!");
+
+exit;
+
+sub _error {
+    my $msg = shift;
+    print STDERR "$msg\n";
+    unlink grep $_ && -f $_, ($path_dest, $path_temp);
     exit 1;
 }
 
-exit;
